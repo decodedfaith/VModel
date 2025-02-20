8585ecce69c3f957c608034163687c7aefb9ed39
decodedfaith <komolafefaith@gmail.com>
2025-02-20 08:58:07 +0100

    -Fixes on PostRow UI component

--- VModel/Views/PostRow.swift
+++ VModel/Views/PostRow.swift
@@ -10,50 +10,68 @@ import SwiftUI
 struct PostRow: View {
     var post: Post
     
-
+    
     var body: some View {
         let firstWord = post.userName.components(separatedBy: " ").first ?? ""
         
         VStack(alignment: .leading) {
             HStack{
-                Image(post.image)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 30, height: 30)
-                        .clipShape(Circle())
+                Image(post.image.first ?? "")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 30, height: 30)
+                    .clipShape(Circle())
                 Text(post.userName)
                 .font(.headline)}
-            Image(post.image)
+            .padding(18)
+            ImageCarouselView(images: post.image)
             
-                Text(firstWord + "  :" + post.content)
-                .font(.body)
+            
             HStack{
                 HStack {
                     Button(action: {}) {
-                        Image(systemName: "heart")
+                        Image("love")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        
                     }
                     Button(action: {}) {
-                        Image(systemName: "bubble.right")
-                    }
-                    Button(action: {}) {
-                        Image(systemName: "arrowshape.turn.up.right")
+                        Image( "icon3")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        
                     }
                 }
                 Spacer()
                 HStack {
                     Button(action: {}) {
-                        Image(systemName: "heart")
+                        Image("icon1")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        
                     }
                     Button(action: {}) {
-                        Image(systemName: "bubble.right")
+                        Image("icon2")
+                            .resizable()
+                            .frame(width: 15, height: 15)
                     }
-                    Button(action: {}) {
-                        Image(systemName: "arrowshape.turn.up.right")
-                    }
+                    
                 }
+            }.padding(2)
+                .padding(.horizontal, 18)
+            HStack {
+                (Text(firstWord)
+                    .font(.footnote)
+                    .bold()
+                 + Text(" " + post.content)
+                    .font(.footnote)
+                )
+                .padding(18)
             }
+            
         }
-        .padding()
+        //        .padding()
+        //        .padding()
         .background(Color.gray.opacity(0.1))
         .cornerRadius(8)
     }
