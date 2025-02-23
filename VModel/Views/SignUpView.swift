431bde10242b1bb0199623450471824008935307
decodedfaith <komolafefaith@gmail.com>
2025-02-23 10:34:16 +0100

    -Signup View UI fixes

--- VModel/Views/SignUpView.swift
+++ VModel/Views/SignUpView.swift
@@ -8,12 +8,18 @@
 import SwiftUI
 
 struct SignUpView: View {
-    @State private var fullName = ""
+    @State private var firstName = ""
+    @State private var lastName = ""
     @State private var email = ""
+    @State private var userName = ""
+    @State private var userType = ""
+    @State private var label = ""
     @State private var password = ""
     @State private var confirmPassword = ""
+    @State private var isBusinessAccount = false
     @StateObject var authViewModel = AuthViewModel()
-
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
         VStack {
             Text("Sign Up")
@@ -21,26 +27,62 @@ struct SignUpView: View {
                 .bold()
                 .padding(.bottom, 20)
             
-            TextField("Full Name", text: $fullName)
+            TextField("First Name", text: $firstName)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding(.horizontal)
-
+            
+            TextField("Last Name", text: $lastName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
+            
             TextField("Email", text: $email)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .keyboardType(.emailAddress)
                 .autocapitalization(.none)
                 .padding(.horizontal)
-
+            
+            TextField("Username", text: $userName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .keyboardType(.emailAddress)
+                .autocapitalization(.none)
+                .padding(.horizontal)
+            TextField("UserType", text: $userType)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
+            
+            TextField("Label", text: $label)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .keyboardType(.emailAddress)
+                .autocapitalization(.none)
+                .padding(.horizontal)
+            Toggle("Is Business Account", isOn: $isBusinessAccount)
+                .padding(.horizontal)
             SecureField("Password", text: $password)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding(.horizontal)
-
+            
             SecureField("Confirm Password", text: $confirmPassword)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding(.horizontal)
+            
+            
+            NavigationLink(destination: FeedView(), isActive: $authViewModel.isAuthenticated) {
+                EmptyView() // Ensures navigation happens when isAuthenticated becomes true
+            }
 
             Button(action: {
-          
+                authViewModel.signUp(
+                    email: email,
+                    username: userName,
+                    firstName: firstName,
+                    lastName: lastName,
+                    userType: userType,
+                    label: label,
+                    isBusinessAccount: isBusinessAccount,
+                    password1: password,
+                    password2: confirmPassword
+                )
+                print("Attempting sign up...")
             }) {
                 Text("Sign Up")
                     .frame(maxWidth: .infinity)
@@ -52,6 +94,8 @@ struct SignUpView: View {
             .padding(.horizontal)
             .padding(.top, 10)
 
+
+            
             HStack {
                 Text("Already have an account?")
                 NavigationLink("Log in", destination: LoginView())
@@ -60,5 +104,18 @@ struct SignUpView: View {
             .padding()
         }
         .padding()
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button(action: { dismiss()
+                }) {
+                    HStack{
+                        Image(systemName: "chevron.left")
+                            .foregroundColor(.black)                            .font(.caption)
+                            .padding(.horizontal, 12)
+                        Spacer()
+                    }}
+            }
+        }
     }
 }
